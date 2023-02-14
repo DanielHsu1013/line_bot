acc7bcf6228ce64b399020777f10654725519beb
Daniel Hsu <danielstark1013@gmail.com>
2023-02-14 20:59:40 +0800

    update

--- app.py
+++ app.py
@@ -50,33 +50,6 @@ def handle_message(event):
     if msg in ['meme', 'MEME', 'Meme', '迷因']:
         r = '請問今天想生產哪種寶寶迷因呢?'
 
-    if msg in ['我想玩猜數字', '猜數字遊戲', '猜數字', '我想玩猜數字遊戲']
-        import random
-        start = input('please enter the start value:')
-        start = int(start)
-        end = input('please enter the end value:')
-        end = int(end)
-
-        x = random.randint(start,end)
-
-        c = 0
-
-        while True:
-            c = c + 1
-            y = input('Guess a number')
-            y = int(y)
-            if y == x:
-                c = c + 1
-                print('you got it! you have guess for', c - 1, 'times')
-                break
-            elif y >= x:
-                 print('guess lower. you have guess for', c, 'times')
-            else:
-                 print('guess higher you have guess for', c, 'times')
-
-
-
-
 
 
 
