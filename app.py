a75137afacce3b302ee090104d79b9e71d912682
Daniel Hsu <danielstark1013@gmail.com>
2023-02-14 18:29:38 +0800

    fix requirements.txt

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = "hi 你好"
+    s = 'hi 你好'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text= s)
