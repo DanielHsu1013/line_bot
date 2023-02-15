1365d70b7d023ac89da51a17c80dad3f15531dbc
Daniel Hsu <danielstark1013@gmail.com>
2023-02-15 21:29:34 +0800

    update

--- app.py
+++ app.py
@@ -228,6 +228,53 @@ def handle_message(event):
 
 
 
+import openai
+
+from flask import Flask, request
+
+# 載入 LINE Message API 相關函式庫
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
+    try:
+        line_bot_api = LineBotApi('txxdJs06LJjEmPkAu1cD0qN6VQWvUbGcDzytZ+VdNEhhjCejas2XqSdnP80F9LbnKL4WZWa1ryDkrmWWUbw5Cjfu1E3L628GqzOjCyOSHgzRCdo8tOlYd3LExUGTQYHTmJZEjo3To1IC6MCeOADz2wdB04t89/1O/w1cDnyilFU=')
+        handler = WebhookHandler('4ed29c44db401ce4d951957b5551d4aa')
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']
+        msg = json_data['events'][0]['message']['text']
+        # 取出文字的前五個字元，轉換成小寫
+        ai_msg = msg[:6].lower()
+        reply_msg = ''
+        # 取出文字的前五個字元是 hi ai:
+        if ai_msg == 'hi ai:':
+            openai.api_key = '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'
+            # 將第六個字元之後的訊息發送給 OpenAI
+            response = openai.Completion.create(
+                engine='text-davinci-003',
+                prompt=msg[6:],
+                max_tokens=256,
+                temperature=0.5,
+                )
+            # 接收到回覆訊息後，移除換行符號
+            reply_msg = response["choices"][0]["text"].replace('\n','')
+        else:
+            reply_msg = msg
+        text_message = TextSendMessage(text=reply_msg)
+        line_bot_api.reply_message(tk,text_message)
+    except:
+        print('error')
+    return 'OK'
+
+
 
 if __name__ == "__main__":
     app.run()
